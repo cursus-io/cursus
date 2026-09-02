e293bec93c573b059f03132aed5f4ea552f63a9d
downfa11 <downfa11@naver.com>
2026-09-02 10:31:03 +0900

    refactor(protocol): use the Wire command registry
    Signed-off-by: downfa11 <downfa11@naver.com>

--- pkg/protocol/commands.go
+++ pkg/protocol/commands.go
@@ -1,58 +1,10 @@
 package protocol
 
-import "strings"
+import (
+	"strings"
 
-const maxTextCommandLength = len("SEND_OFFSETS_TO_TXN")
-
-var textCommands = map[string]struct{}{
-	"AUTH":                {},
-	"CREATE":              {},
-	"ALTER_TOPIC_CONFIG":  {},
-	"DELETE":              {},
-	"TRUNCATE":            {},
-	"LIST":                {},
-	"LIST_CLUSTER":        {},
-	"CLUSTER_STATUS":      {},
-	"ELECT_LEADER":        {},
-	"PUBLISH":             {},
-	"CONSUME":             {},
-	"STREAM":              {},
-	"HELP":                {},
-	"HEARTBEAT":           {},
-	"JOIN_GROUP":          {},
-	"LEAVE_GROUP":         {},
-	"COMMIT_OFFSET":       {},
-	"BATCH_COMMIT":        {},
-	"REGISTER_GROUP":      {},
-	"GROUP_STATUS":        {},
-	"FETCH_OFFSET":        {},
-	"LIST_GROUPS":         {},
-	"LIST_OFFSETS":        {},
-	"SYNC_GROUP":          {},
-	"DESCRIBE":            {},
-	"INIT_PRODUCER_ID":    {},
-	"BEGIN_TXN":           {},
-	"TXN_PUBLISH":         {},
-	"SEND_OFFSETS_TO_TXN": {},
-	"END_TXN":             {},
-	"TXN_STATUS":          {},
-	"APPEND_STREAM":       {},
-	"READ_STREAM":         {},
-	"SAVE_SNAPSHOT":       {},
-	"READ_SNAPSHOT":       {},
-	"STREAM_VERSION":      {},
-	"REPLICATE_MESSAGE":   {},
-	"REPLICATE_SNAPSHOT":  {},
-	"LIST_SNAPSHOTS":      {},
-	"FETCH_SNAPSHOT":      {},
-	"CATCHUP_SNAPSHOTS":   {},
-	"FIND_COORDINATOR":    {},
-	"RAFT_APPLY":          {},
-	"METADATA":            {},
-	"INTERNAL_BATCH":      {},
-	"PROTOCOL_INFO":       {},
-	"NEGOTIATE":           {},
-}
+	"github.com/cursus-io/cursus/pkg/wire"
+)
 
 // IsTextCommand reports whether input starts with a registered command token.
 func IsTextCommand(value string) bool {
@@ -64,17 +16,18 @@ func IsTextCommand(value string) bool {
 	if (first < 'A' || first > 'Z') && (first < 'a' || first > 'z') {
 		return false
 	}
-	searchEnd := len(trimmed)
-	if searchEnd > maxTextCommandLength+1 {
-		searchEnd = maxTextCommandLength + 1
-	}
-	end := strings.IndexAny(trimmed[:searchEnd], " \t\r\n")
+	end := strings.IndexAny(trimmed, " \t\r\n")
 	if end == -1 {
-		if len(trimmed) > maxTextCommandLength {
-			return false
-		}
 		end = len(trimmed)
 	}
-	_, ok := textCommands[strings.ToUpper(trimmed[:end])]
-	return ok
+	command, err := wire.ParseCommand(trimmed[:end])
+	if err != nil {
+		return false
+	}
+	switch command {
+	case wire.CommandJoinCluster, wire.CommandLeaveCluster, wire.CommandHeartbeatCluster:
+		return false
+	default:
+		return true
+	}
 }
